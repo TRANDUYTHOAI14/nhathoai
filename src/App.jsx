67c21a163b86bade2db1092edcf41d94c9102be1
duy thoai <duythoai.gooweb@gmail.com>
2022-09-29 09:40:22 +0700

    29 / 9 - Ngày 3

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,14 @@
 import React from "react";
-export default function App() {
-  const name = 'thoai';
-  const old = 20;
-  const isFemale = true;
-  const student = {
-    name: 'HTML',
-  }
-  const colorList = [
-    'red','blue','green'
-  ]
+import "./App.css";
+import BoxComponent from "./boxList";
 
- return (
-  <div className="app">
-   <header>
-    <ul>
-      <li>{name}</li>
-      <li>{old}</li>
-    </ul>
-   </header>
-   <main>
-    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Velit ab qui, harum alias repudiandae libero impedit possimus. Dolores pariatur minus eius nihil accusamus sequi quis, est eveniet eos maiores illum.</p>
-   </main>
-   <footer>
-    {
-      colorList.map((item) =>
-        <li>{item}</li>
-      )
-    }
-    <p>Lorem, ipsum dolor.</p>
-   </footer>
-  </div>
- );
+function App(props) {
+  return (
+    <div className="app">
+      <h1>Hello wworld</h1>
+      <BoxComponent />
+    </div>
+  );
 }
 
+export default App;
